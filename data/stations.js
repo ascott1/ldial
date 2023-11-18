f44c7d759173f2b32616258bc4e4ec87b92bc260
Adam Scott <ascott1@gmail.com>
2023-11-18 17:44:09 -0500

    minor station tweaks

--- data/stations.js
+++ data/stations.js
@@ -1,81 +1,88 @@
 // /data/stations.js
 const stations = [
   {
-    id: 1,
+    id: "WFMU",
     name: "WFMU 91.1 FM - Jersey City, NJ",
     description:
       "An independent freeform station broadcasting at 91.1 fm in New York, known for its eclectic mix of music and programming.",
     streamUrl: "http://stream0.wfmu.org/freeform-128k",
   },
   {
-    id: 2,
+    id: "KEXP",
     name: "KEXP 90.3 FM - Seattle, WA",
     description:
       "Famous for its live studio sessions, KEXP is a haven for lovers of indie rock, alternative music, and beyond.",
     streamUrl: "http://live-mp3-128.kexp.org/kexp128.mp3",
   },
   {
-    id: 3,
+    id: "WXPN",
     name: "WXPN 88.5 FM - Philadelphia, PA",
     description:
       "A non-commercial, public FM radio station known for an eclectic range of music, including indie rock, folk, and blues.",
     streamUrl: "https://wxpn.xpn.org/xpnmp3hi",
   },
   {
-    id: 4,
+    id: "KCRW",
     name: "KCRW 89.9 FM - Santa Monica, CA",
     description:
       "Offers a diverse range of music genres, from indie rock to world music, along with news and cultural programming.",
     streamUrl: "http://kcrw.streamguys1.com/kcrw_192k_mp3_on_air",
   },
   {
-    id: 5,
+    id: "KPFA",
     name: "KPFA 94.1 FM - Berkeley, CA",
     description:
       "The first listener-supported radio station in the USA, known for its progressive perspectives and diverse music programming.",
     streamUrl: "http://stream.kpfa.org:8000/kpfa_64",
   },
   {
-    id: 6,
+    id: "WHRB",
     name: "WHRB 95.3 FM - Cambridge, MA",
     description:
-      'Run by Harvard students, this station is known for its "orgies" – marathon sessions dedicated to specific artists or genres.',
+      "WHRB, Harvard Radio Broadcasting, is a student-run radio station at Harvard University offering a wide range of programming, from classical music to underground rock and jazz.",
     streamUrl: "https://stream.whrb.org/whrb-he-aac",
   },
   {
-    id: 7,
+    id: "KFAI",
     name: "KFAI 90.3 FM - Minneapolis, MN",
     description:
       "A community radio station with a wide variety of programming, reflecting the diverse communities of the Twin Cities.",
     streamUrl: "https://kfai.broadcasttool.stream/kfai-1",
   },
   {
-    id: 8,
+    id: "WXYC",
     name: "WXYC 89.3 FM - Chapel Hill, NC",
     description:
       "A student-run station known for its eclectic and experimental music choices, reflecting the diverse tastes of its DJs.",
     streamUrl: "http://audio-mp3.ibiblio.org:8000/wxyc.mp3",
   },
   {
-    id: 9,
+    id: "KFJC",
     name: "KFJC 89.7 FM - Los Altos Hills, CA",
     description:
       "Based at Foothill College, known for adventurous programming and support of underground and experimental music.",
     streamUrl: "https://netcast.kfjc.org/kfjc-128k-mp3",
   },
   {
-    id: 10,
+    id: "KMKB",
     name: "KMKB 98.9 FM - Marfa, TX",
     description:
       "KMKB 98.9 is a community radio station and recording studio in Marfa, TX.",
     streamUrl: "http://50.19.66.66:8000/kmkb",
   },
   {
-    id: 11,
+    id: "WWOZ",
     name: "WWOZ 90.7 FM  - New Orleans, LA",
     description:
-      "WWOZ (90.7 FM) is a non-profit community-supported radio station in New Orleans. The station specializes in music from or relating to the cultural heritage of New Orleans and the surrounding region of Louisiana.",
+      "WWOZ, based in New Orleans, is a listener-supported, volunteer-programmed community radio station known for its deep roots in jazz, blues, and local music culture.",
     streamUrl: "https://wwoz-sc.streamguys1.com/wwoz-hi.mp3",
+  },
+  {
+    id: "WDCB",
+    name: "WDCB 90.9 FM - Chicago, IL",
+    description:
+      "WDCB 90.9 FM is a non-commercial public FM radio station serving the Chicago metropolitan area. It largely airs jazz programming and is owned by the College of DuPage.",
+    streamUrl: "https://wdcb-ice.streamguys1.com/mobile-AAC",
   },
 ];
 
