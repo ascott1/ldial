55764689ab3518cb9f5f9d97544226ef57498f32
Adam Scott <ascott1@gmail.com>
2023-11-19 10:41:14 -0500

    bug fixes for url proxying

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,22 +1,21 @@
 /* eslint-disable import/no-anonymous-default-export */
-// pages/api/proxy/[...path].js
+// pages/api/proxy.js
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 export default function (req, res) {
-  // Don't allow requests to just any URL, make sure to validate the URL being requested
-  if (req.query.url.startsWith("http://50.19.66.66:8000")) {
+  // Validate the URL being requested
+  if (req.query.url && req.query.url.startsWith("http://50.19.66.66:8000")) {
     const decodedUrl = decodeURIComponent(req.query.url);
     return createProxyMiddleware({
       target: decodedUrl,
       changeOrigin: true,
+      followRedirects: true,
       pathRewrite: {
         "^/api/proxy": "",
       },
       onProxyRes: function (proxyRes, req, res) {
-        if (!proxyRes.headers["content-type"]) {
-          // Set content-type header if not already set by the server
-          proxyRes.headers["content-type"] = "audio/mpeg";
-        }
+        // Overwrite the content-type header
+        proxyRes.headers["content-type"] = "audio/mpeg";
       },
       // Additional options here if needed
     })(req, res);
