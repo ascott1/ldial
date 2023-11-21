a9a13d66ef89229ed876e86d732346ce76444cc5
Adam Scott <ascott1@gmail.com>
2023-11-21 17:43:35 -0500

    add language attribute

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const withPWA = require("next-pwa")({
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 });
 
 module.exports = nextConfig;
