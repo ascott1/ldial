300732096ba733c3a957f01d181eab1351c569cc
Adam Scott <ascott1@gmail.com>
2023-12-21 16:32:46 -0500

    add a few more stations

--- data/stations.js
+++ data/stations.js
@@ -6,6 +6,8 @@ const stations = [
     description:
       "An independent freeform station broadcasting at 91.1 fm in New York, known for its eclectic mix of music and programming.",
     streamUrl: "https://stream0.wfmu.org/freeform-128k",
+    tags: ["indie", "rock"],
+    timezone: "EST",
   },
   {
     id: "KEXP",
@@ -13,6 +15,8 @@ const stations = [
     description:
       "Famous for its live studio sessions, KEXP is a haven for lovers of indie rock, alternative music, and beyond.",
     streamUrl: "https://kexp.streamguys1.com/kexp160.aac",
+    tags: ["indie", "rock"],
+    timezone: "PST",
   },
   {
     id: "WXPN",
@@ -20,6 +24,8 @@ const stations = [
     description:
       "A non-commercial, public FM radio station known for an eclectic range of music, including indie rock, folk, and blues.",
     streamUrl: "https://wxpn.xpn.org/xpnmp3hi",
+    tags: ["indie", "rock"],
+    timezone: "EST",
   },
   {
     id: "KCRW",
@@ -27,6 +33,8 @@ const stations = [
     description:
       "Offers a diverse range of music genres, from indie rock to world music, along with news and cultural programming.",
     streamUrl: "https://kcrw.streamguys1.com/kcrw_192k_mp3_on_air",
+    tags: ["indie", "rock"],
+    timezone: "PST",
   },
   {
     id: "KPFA",
@@ -34,6 +42,8 @@ const stations = [
     description:
       "The first listener-supported radio station in the USA, known for its progressive perspectives and diverse music programming.",
     streamUrl: "http://stream.kpfa.org:8000/kpfa_64",
+    tags: ["indie", "rock"],
+    timezone: "PST",
   },
   {
     id: "WHRB",
@@ -41,6 +51,8 @@ const stations = [
     description:
       "WHRB, Harvard Radio Broadcasting, is a student-run radio station at Harvard University offering a wide range of programming, from classical music to underground rock and jazz.",
     streamUrl: "https://stream.whrb.org/whrb-he-aac",
+    tags: ["indie", "rock", "classical"],
+    timezone: "EST",
   },
   {
     id: "KFAI",
@@ -48,6 +60,8 @@ const stations = [
     description:
       "A community radio station with a wide variety of programming, reflecting the diverse communities of the Twin Cities.",
     streamUrl: "https://kfai.broadcasttool.stream/kfai-1",
+    tags: ["indie", "rock"],
+    timezone: "CST",
   },
   {
     id: "WXYC",
@@ -55,6 +69,8 @@ const stations = [
     description:
       "A student-run station known for its eclectic and experimental music choices, reflecting the diverse tastes of its DJs.",
     streamUrl: "https://audio-mp3.ibiblio.org/wxyc.mp3",
+    tags: ["indie", "rock"],
+    timezone: "EST",
   },
   {
     id: "KFJC",
@@ -62,6 +78,8 @@ const stations = [
     description:
       "Based at Foothill College, known for adventurous programming and support of underground and experimental music.",
     streamUrl: "https://netcast.kfjc.org/kfjc-128k-mp3",
+    tags: ["indie", "rock"],
+    timezone: "PST",
   },
   {
     id: "KMKB",
@@ -69,6 +87,8 @@ const stations = [
     description:
       "KMKB 98.9 is a community radio station and recording studio in Marfa, TX.",
     streamUrl: "https://ldial-proxy.up.railway.app/kmkb",
+    tags: ["indie", "rock"],
+    timezone: "CST",
   },
   {
     id: "WWOZ",
@@ -76,6 +96,8 @@ const stations = [
     description:
       "WWOZ, based in New Orleans, is a listener-supported, volunteer-programmed community radio station known for its deep roots in jazz, blues, and local music culture.",
     streamUrl: "https://wwoz-sc.streamguys1.com/wwoz-hi.mp3",
+    tags: ["jazz", "blues"],
+    timezone: "CST",
   },
   {
     id: "WDCB",
@@ -83,6 +105,44 @@ const stations = [
     description:
       "WDCB 90.9 FM is a non-commercial public FM radio station serving the Chicago metropolitan area. It largely airs jazz programming and is owned by the College of DuPage.",
     streamUrl: "https://wdcb-ice.streamguys1.com/mobile-AAC",
+    tags: ["jazz"],
+    timezone: "CST",
+  },
+  {
+    id: "WXYR",
+    name: "WXYR 91.7 FM - Memphis, TN",
+    description:
+      "WYXR (91.7 FM) is an eclectic, non-profit community radio station headquartered in the historic Crosstown Concourse building in Memphis, Tennessee.",
+    streamUrl: "https://crosstown.streamguys1.com/live-website-aac",
+    tags: ["indie", "eclectic"],
+    timezone: "CST",
+  },
+  {
+    id: "WXNA",
+    name: "WXNA 101.5 FM - Nashville, TN",
+    description:
+      "WXNA is a freeform, listener-supported radio station created by and for the community to provide diverse and innovative programming that reflects the music, culture, concerns, and interests of Nashville and the surrounding area.",
+    streamUrl: "http://listen.wxnafm.org:8000/stream",
+    tags: ["indie", "eclectic"],
+    timezone: "CST",
+  },
+  {
+    id: "WYEP",
+    name: "WYEP 91.3 FM - Pittsburgh, PA",
+    description:
+      "91.3 WYEP is Pittsburgh's independent public radio, dedicated to enriching the community through music discovery, expression, & education.",
+    streamUrl: "https://ais-sa3.cdnstream1.com/2557_128.aac",
+    tags: ["indie", "eclectic"],
+    timezone: "EST",
+  },
+  {
+    id: "WKCR",
+    name: "WKCR 89.9 FM - New York, NY",
+    description:
+      "WKCR is Columbia University's non-commercial student-run radio station, dedicated to presenting a spectrum of alternative programming—traditional and art music, spoken arts, and original journalism.",
+    streamUrl: "https://wkcr.streamguys1.com/live",
+    tags: ["indie", "eclectic", "news"],
+    timezone: "EST",
   },
 ];
 
