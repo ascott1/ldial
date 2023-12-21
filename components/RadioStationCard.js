f11f9bc2ddc2c4d8d08bd2e049b907372025a41a
Adam Scott <ascott1@gmail.com>
2023-12-21 16:48:15 -0500

    add favorite button

--- components/RadioStationCard.js
+++ components/RadioStationCard.js
@@ -1,16 +1,31 @@
 import Head from "next/head";
 import styles from "./RadioStationCard.module.css"; // Assuming you're using CSS Modules
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
-const RadioStationCard = ({ station, isPlaying, onSelect }) => {
+const RadioStationCard = ({
+  station,
+  isPlaying,
+  onSelect,
+  onFavoriteToggle,
+  isFavorite,
+}) => {
   const cardClass = isPlaying
     ? `${styles.card} ${styles.playing}`
     : `${styles.card}`;
+
+  const handleFavoriteClick = (e) => {
+    e.stopPropagation(); // Prevents the onSelect event when clicking the favorite button
+    onFavoriteToggle(station.id);
+  };
 
   return (
     <div className={cardClass} onClick={() => onSelect(station)}>
       <h2 className={styles.stationName}>{station.name}</h2>
       <p className={styles.stationDescription}>{station.description}</p>
-      {/* Add more station details here */}
+      <button onClick={handleFavoriteClick} className={styles.favoriteButton}>
+        {isFavorite ? <FaHeart /> : <FaRegHeart />}{" "}
+        {/* Change these to actual icons if needed */}
+      </button>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import stations from "../data/stations";
 import PageTitle from "../components/PageTitle";
 import RadioStationCard from "../components/RadioStationCard";
@@ -9,36 +9,48 @@ import styles from "../styles/Home.module.css";
 const IndexPage = () => {
   const [currentStation, setCurrentStation] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    // Load favorites from localStorage on component mount
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
 
   const handleStationSelect = (station) => {
     setCurrentStation(station);
     setIsPlaying(true);
   };
 
+  const handleFavoriteToggle = (stationId) => {
+    const updatedFavorites = favorites.includes(stationId)
+      ? favorites.filter((id) => id !== stationId)
+      : [...favorites, stationId];
+
+    setFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
+
+  const sortedStations = [...stations].sort((a, b) => {
+    if (favorites.includes(a.id) && !favorites.includes(b.id)) return -1;
+    if (!favorites.includes(a.id) && favorites.includes(b.id)) return 1;
+    return a.id.localeCompare(b.id);
+  });
+
   return (
     <div>
       <PageTitle />
       <div className={styles.stations}>
-        {stations
-          // Sort stations by ID
-          .sort((a, b) => {
-            if (a.id < b.id) {
-              return -1;
-            }
-            if (a.id > b.id) {
-              return 1;
-            }
-            return 0;
-          })
-          // Map over stations and render a RadioStationCard for each one
-          .map((station) => (
-            <RadioStationCard
-              key={station.id}
-              station={station}
-              isPlaying={currentStation?.id === station.id && isPlaying}
-              onSelect={handleStationSelect}
-            />
-          ))}
+        {sortedStations.map((station) => (
+          <RadioStationCard
+            key={station.id}
+            station={station}
+            isPlaying={currentStation?.id === station.id && isPlaying}
+            onSelect={handleStationSelect}
+            onFavoriteToggle={handleFavoriteToggle}
+            isFavorite={favorites.includes(station.id)}
+          />
+        ))}
       </div>
       {currentStation && (
         <AudioPlayer
